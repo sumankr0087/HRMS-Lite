35665eefd9f863daf741e4b587b6552d26a5559b
sumankr0087 <suman.kumar@essentia.dev>
2026-02-06 19:59:24 +0530

    fix cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import employee, attendance
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="HRMS Lite API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",   # local dev
+        "https://your-frontend-url.vercel.app"  # production (optional)
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(employee.router)
 app.include_router(attendance.router)
